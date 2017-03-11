ad313d0d492db953ca61f8e29d9fb2787a4f1fab
Marc Autord <marc.autord@haarukka.net>
2017-03-11 17:14:46 +0100

    Page joueurs avec chargement asynchrone

--- app.js
+++ app.js
@@ -14,28 +14,28 @@ var docClient = new AWS.DynamoDB.DocumentClient()
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/api/equipes', function(req, res) {
-  var table = "Equipe";
+app.get('/api/joueurs', function(req, res) {
+  var table = "Joueur";
   var params = {
-      TableName: table,
-      Key:{
-          "annee": 2017,
-          "nom_equipe": "Les Castors"
-      }
+      TableName: table
+      // Key:{
+      //     "annee": 2017,
+      //     "nom_equipe": "Les Castors"
+      // }
   };
 
-  docClient.get(params, function(err, data) {
+  docClient.scan(params, function(err, data) {
       if (err) {
           console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
       } else {
-          console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+          console.log("Succès : requête pour les joueurs !");
           res.json(data)
       }
   });
 });
 
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
         res.sendfile('./public/index.html');
 });
 
--- public/core.js
+++ public/core.js
@@ -1,15 +1,23 @@
 angular
-  .module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'ngRoute'])
+  .module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache', 'ngRoute', 'ngResource'])
   .config(['$routeProvider', '$locationProvider',
   function($routeProvider) {
     $routeProvider
-      .when('/:page', {
-        templateUrl: function(params){return '/views/' + params.page + '.html'},
+      .when('/joueurs', {
+        templateUrl: '/views/joueurs.html',
         controller: 'JoueursCtrl'
         })
       .when('/', { templateUrl: '/views/main.html'})
       .otherwise({ redirectTo: '/'});
     }])
+  .factory('joueurs', function($http, $q, $resource){
+    return {
+      data: function() {
+        var deferred = $q.defer();
+        $resource('/api/joueurs').get({},
+                                      function(r) {deferred.resolve(r);},
+                                      deferred.reject);
+        return deferred.promise;}}})
   .controller('AppCtrl', function ($scope, $mdSidenav) {
     $scope.toShow = "home";
 
@@ -25,6 +33,13 @@ angular
     $scope.show = function (toShow) {
       $scope.toShow = toShow;
     }})
-  .controller('JoueursCtrl', ['$scope', function($scope) {
+  .controller('JoueursCtrl', function($scope, joueurs) {
     $scope.page = "JOUEURS";
-}]);
+    $scope.joueurs = "Chargement...";
+    joueurs.data().then(function(res){
+      $scope.joueurs = res.Items;
+    }, function(err){
+      $scope.joueurs = "Erreur dans le chargement...";
+    });
+    //console.log($scope.joueurs)
+});
